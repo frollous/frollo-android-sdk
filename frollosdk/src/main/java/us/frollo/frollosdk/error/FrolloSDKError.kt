c74a865e863571971a95b266de2c57fbe0ececeb
Deepak <deepak@frollo.us>
2018-12-21 11:33:15 +1100

    Frollo SDK Tests - Resource Tests

--- frollosdk/src/main/java/us/frollo/frollosdk/error/FrolloSDKError.kt
+++ frollosdk/src/main/java/us/frollo/frollosdk/error/FrolloSDKError.kt
@@ -1,11 +1,12 @@
 package us.frollo.frollosdk.error
 
+import android.content.Context
 import us.frollo.frollosdk.FrolloSDK
 import us.frollo.frollosdk.R
 
 open class FrolloSDKError(errorMessage: String? = null) {
 
-    protected val context = FrolloSDK.app.applicationContext
+    protected val context: Context = FrolloSDK.app.applicationContext
 
     open val localizedDescription: String =
             errorMessage ?: context.resources.getString(R.string.Error_Generic_UnknownError)

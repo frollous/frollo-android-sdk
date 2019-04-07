d97f022917fb48d09c4082b97e00d4105f742b94
Deepak Marasandra <deepak@frollo.us>
2019-04-08 09:19:59 +1000

    [SDK-146] SDK - Database Migration - DB version 1 to 2

--- src/main/kotlin/us/frollo/frollosdk/database/SDKDatabase.kt
+++ src/main/kotlin/us/frollo/frollosdk/database/SDKDatabase.kt
@@ -21,6 +21,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import us.frollo.frollosdk.database.dao.*
 import us.frollo.frollosdk.model.api.messages.MessageResponse
 import us.frollo.frollosdk.model.coredata.aggregation.accounts.Account
@@ -48,7 +50,7 @@ import us.frollo.frollosdk.model.coredata.user.User
     ReportTransactionHistory::class,
     ReportGroupTransactionHistory::class,
     ReportAccountBalance::class
-], version = 1, exportSchema = true)
+], version = 2, exportSchema = true)
 
 @TypeConverters(Converters::class)
 abstract class SDKDatabase : RoomDatabase() {
@@ -80,16 +82,36 @@ abstract class SDKDatabase : RoomDatabase() {
         private fun create(app: Application): SDKDatabase =
                 Room.databaseBuilder(app, SDKDatabase::class.java, DATABASE_NAME)
                         .allowMainThreadQueries() // Needed for some tests
-                        .fallbackToDestructiveMigration()
-                        //.addMigrations(MIGRATION_1_2)
+                        //.fallbackToDestructiveMigration()
+                        .addMigrations(MIGRATION_1_2)
                         .build()
 
         // Copy-paste of auto-generated SQLs from room schema json file
-        // located in sandbox code after building under app/schemas/$version.json
-        /*private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+        // located in sandbox code after building under frollo-android-sdk/schemas/$version.json
+        private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                //to be implemented
+                // New changes in this migration:
+                // 1) Add new table/entity report_transaction_current and create indexes
+                // 2) Add new table/entity report_transaction_history and create indexes
+                // 3) Add new table/entity report_group_transaction_history and create indexes
+                // 4) Add new table/entity report_account_balance and create indexes
+
+                database.execSQL("CREATE TABLE IF NOT EXISTS `report_transaction_current` (`report_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `day` INTEGER NOT NULL, `linked_id` INTEGER, `linked_name` TEXT, `spend_value` TEXT, `previous_period_value` TEXT, `average_value` TEXT, `budget_value` TEXT, `filtered_budget_category` TEXT, `report_grouping` TEXT NOT NULL)")
+                database.execSQL("CREATE INDEX `index_report_transaction_current_report_id` ON `report_transaction_current` (`report_id`)")
+                database.execSQL("CREATE UNIQUE INDEX `index_report_transaction_current_linked_id_day_filtered_budget_category_report_grouping` ON `report_transaction_current` (`linked_id`, `day`, `filtered_budget_category`, `report_grouping`)")
+
+                database.execSQL("CREATE TABLE IF NOT EXISTS `report_transaction_history` (`report_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `date` TEXT NOT NULL, `value` TEXT NOT NULL, `budget` TEXT, `period` TEXT NOT NULL, `filtered_budget_category` TEXT, `report_grouping` TEXT NOT NULL)")
+                database.execSQL("CREATE INDEX `index_report_transaction_history_report_id` ON `report_transaction_history` (`report_id`)")
+                database.execSQL("CREATE UNIQUE INDEX `index_report_transaction_history_date_period_filtered_budget_category_report_grouping` ON `report_transaction_history` (`date`, `period`, `filtered_budget_category`, `report_grouping`)")
+
+                database.execSQL("CREATE TABLE IF NOT EXISTS `report_group_transaction_history` (`report_group_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `linked_id` INTEGER NOT NULL, `linked_name` TEXT NOT NULL, `value` TEXT NOT NULL, `budget` TEXT, `date` TEXT NOT NULL, `period` TEXT NOT NULL, `filtered_budget_category` TEXT, `report_grouping` TEXT NOT NULL, `transaction_ids` TEXT, `report_id` INTEGER NOT NULL)")
+                database.execSQL("CREATE INDEX `index_report_group_transaction_history_report_group_id` ON `report_group_transaction_history` (`report_group_id`)")
+                database.execSQL("CREATE UNIQUE INDEX `index_report_group_transaction_history_linked_id_date_period_filtered_budget_category_report_grouping` ON `report_group_transaction_history` (`linked_id`, `date`, `period`, `filtered_budget_category`, `report_grouping`)")
+
+                database.execSQL("CREATE TABLE IF NOT EXISTS `report_account_balance` (`report_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `date` TEXT NOT NULL, `account_id` INTEGER NOT NULL, `currency` TEXT NOT NULL, `value` TEXT NOT NULL, `period` TEXT NOT NULL)")
+                database.execSQL("CREATE INDEX `index_report_account_balance_report_id` ON `report_account_balance` (`report_id`)")
+                database.execSQL("CREATE UNIQUE INDEX `index_report_account_balance_account_id_date_period` ON `report_account_balance` (`account_id`, `date`, `period`)")
             }
-        }*/
+        }
     }
 }

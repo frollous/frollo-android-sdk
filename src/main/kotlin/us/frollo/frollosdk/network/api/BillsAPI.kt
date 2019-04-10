7eb08810af1526ee5679e210d46231aed35a260c
Deepak Marasandra <deepak@frollo.us>
2019-04-10 12:02:11 +1000

    [SDK-124] [SDK-127] SDK - Bills & Bill Payments - API

---
+++ src/main/kotlin/us/frollo/frollosdk/network/api/BillsAPI.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2019 Frollo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package us.frollo.frollosdk.network.api
+
+import retrofit2.Call
+import retrofit2.http.*
+import us.frollo.frollosdk.model.api.bills.*
+
+internal interface BillsAPI {
+    companion object {
+        // Bill URLs
+        const val URL_BILLS = "aggregation/bills"
+        const val URL_BILL = "aggregation/bills/{bill_id}"
+
+        // Bill Payment URLs
+        const val URL_BILL_PAYMENTS = "bills/payments"
+        const val URL_BILL_PAYMENT = "bills/payments/{bill_payment_id}"
+    }
+
+    // Bill API
+
+    @GET(URL_BILLS)
+    fun fetchBills(): Call<BillsResponse>
+
+    @GET(URL_BILL)
+    fun fetchBill(@Path("bill_id") billId: Long): Call<BillResponse>
+
+    @POST(URL_BILLS)
+    fun createBill(@Body request: BillCreateRequest): Call<BillResponse>
+
+    @PUT(URL_BILL)
+    fun updateBill(@Path("bill_id") billId: Long, @Body request: BillUpdateRequest): Call<BillResponse>
+
+    @DELETE(URL_BILL)
+    fun deleteBill(@Path("bill_id") billId: Long): Call<Void>
+
+    // Bill Payment API
+
+    // Query parameters: {from_date, to_date, skip, count}
+    @GET(URL_BILL_PAYMENTS)
+    fun fetchBillPayments(@QueryMap queryParams: Map<String, String>): Call<List<BillPaymentResponse>>
+
+    @GET(URL_BILL_PAYMENT)
+    fun fetchBillPayment(@Path("bill_payment_id") billPaymentId: Long): Call<BillPaymentResponse>
+
+    @PUT(URL_BILL_PAYMENT)
+    fun updateBillPayment(@Path("bill_payment_id") billPaymentId: Long, @Body request: BillPaymentUpdateRequest): Call<BillPaymentResponse>
+
+    @DELETE(URL_BILL_PAYMENT)
+    fun deleteBillPayment(@Path("bill_payment_id") billPaymentId: Long): Call<Void>
+}

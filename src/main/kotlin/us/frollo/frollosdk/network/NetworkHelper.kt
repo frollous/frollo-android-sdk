180ae2fa7f899758d5e37ac7a817ed4d7aa4c4bb
Deepak Marasandra <deepak@frollo.us>
2019-07-26 15:08:16 +1000

    Roll back to use Host API version 2.2 until backend finds a way to handle client ID for Auth0 tenants.

--- src/main/kotlin/us/frollo/frollosdk/network/NetworkHelper.kt
+++ src/main/kotlin/us/frollo/frollosdk/network/NetworkHelper.kt
@@ -34,7 +34,7 @@ internal class NetworkHelper(private val authToken: AuthToken) {
         internal const val HEADER_DEVICE_VERSION = "X-Device-Version"
         internal const val HEADER_API_VERSION = "X-Api-Version"
         internal const val HEADER_BACKGROUND = "X-Background"
-        internal const val API_VERSION = "2.4"
+        internal const val API_VERSION = "2.2"
     }
 
     internal val authAccessToken: String?

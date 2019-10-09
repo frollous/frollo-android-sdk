8fce61939703d4b640e398c87fb586dbffae8c5b
Kartik Shah <kartik@frollo.us>
2019-10-09 11:07:59 +1100

    SDK-294 budget category apply all changes

--- src/main/kotlin/us/frollo/frollosdk/model/api/aggregation/transactions/TransactionUpdateRequest.kt
+++ src/main/kotlin/us/frollo/frollosdk/model/api/aggregation/transactions/TransactionUpdateRequest.kt
@@ -26,5 +26,6 @@ internal data class TransactionUpdateRequest(
     @SerializedName("memo") val memo: String? = null,
     @SerializedName("user_description") val userDescription: String? = null,
     @SerializedName("recategorise_all") val recategoriseAll: Boolean? = null,
+    @SerializedName("budget_apply_all") val budgetCategoryApplyAll: Boolean? = null,
     @SerializedName("include_apply_all") val includeApplyAll: Boolean? = null
 )
